069734a8d88c8e9a2bfaf661f8d7e62cffa3942a
gabrielbecchi <gabrielbecchi97@gmail.com>
2015-08-06 22:21:29 -0300

    Create funcoesshell.py

---
+++ DFG/tools/funcoesshell.py
@@ -0,0 +1,48 @@
+#Funcao que recebe o nome do arquivo e busca as 10 palavras principais (mais repetidas) deste arquivo
+#Cria como saida um texto com o a palavra e o numero de vezes em que esta palavra apareceu
+def palavrasPrincipais (ArquivoEntrada, address):
+	import os.path
+	if (os.path.exists("temp/MostUsedWords") == False):
+		os.makedirs(address + "temp/MostUsedWords")
+	try:
+		os.system("cut -f 1 RD/"+ArquivoEntrada+".txt | sort | uniq -c | sort -g | tail > temp/MostUsedWords/"+ArquivoEntrada+".txt")
+	except:
+			return False
+	return True
+
+#Recebe o endereco e cria um arquivo na pasta temp como o nome de todos os arquivos dentro da pasta ENTERFILES
+#address = Endereco do Main
+#TargetDir = diretorio alvo (sem / no final)
+def nameAllfiles (address, TargetDir, exitname):
+	import os.path
+	numberoArq = 0
+	try:
+		os.system("ls "+TargetDir+"/ -lit > temp/allfilesterminal.txt")
+		newfile = open(address+"/temp/"+exitname+".txt", "w")
+		arquivoEntrada = open(address+"/temp/allfilesterminal.txt", 'r')
+	except:
+		return 0
+	for conteudo_linhaAtual in arquivoEntrada:
+		if (conteudo_linhaAtual[0] == "t"):
+			conteudo_linhaAtual.strip()
+		else:
+			posicao = conteudo_linhaAtual.rfind(" ")
+			newfile.write(conteudo_linhaAtual[posicao+1:])
+			conteudo_linhaAtual.strip()
+	os.remove(address+"/temp/allfilesterminal.txt")
+	return numberoArq
+
+#Localiza as palavras de um texto e retorna uma lista com a posicao de cada palavra
+def achaPosicaoPalavraNoTexto (palavra, arquivoTarget ,address):
+	try:
+		os.system("grep "+palavra+" RD/"+arquivoTarget+" > temp/posicao.txt")
+		arquivoEntrada = open(address+"/temp/posicao.txt", 'r')
+	except:
+		return False
+	positionList = []
+	for conteudo_linhaAtual in arquivoEntrada:
+		posicao = conteudo_linhaAtual.rfind("\t")
+			positionList.append(conteudo_linhaAtual[posicao+1:])
+			conteudo_linhaAtual.strip()
+	os.remove(address+"/temp/posicao.txt")
+	return positionList
