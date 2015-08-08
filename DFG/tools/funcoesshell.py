635215ae299878cff1bd41fdb8e196e96c100f7b
gabrielbecchi <gabrielbecchi97@gmail.com>
2015-08-08 17:02:38 -0300

    Update funcoesshell.py

--- DFG/tools/funcoesshell.py
+++ DFG/tools/funcoesshell.py
@@ -2,12 +2,13 @@
 #Cria como saida um texto com o a palavra e o numero de vezes em que esta palavra apareceu
 def palavrasPrincipais (ArquivoEntrada, address):
 	import os.path
-	os.makedirs(address + "/temp/MostUsedWords", 777)
-	try:
-		os.system("cut -f 1 RD/"+ArquivoEntrada+".txt | sort | uniq -c | sort -g | tail > temp/MostUsedWords/"+ArquivoEntrada+".txt")
-	except:
-		return False
-	return True
+	os.system("cut -f 1 temp/RD/"+ArquivoEntrada+" | sort | uniq -c | sort -g | tail > temp/MostUsedWordsFILE.txt")
+	File1 = open(address+"/temp/MostUsedWordsFILE.txt")
+	newfile = open(address+"/temp/MostUsedWords_"+ArquivoEntrada, "w+")
+	for linhaatual in File1:
+		posicao = linhaatual.rfind(" ")
+		newfile.write(linhaatual[posicao+1:])
+	os.remove(address+"/temp/MostUsedWordsFILE.txt")
 
 #Recebe o endereco e cria um arquivo na pasta temp como o nome de todos os arquivos dentro da pasta ENTERFILES
 #address = Endereco do Main
@@ -33,16 +34,17 @@ def nameAllfiles (address, TargetDir, exitname):
 	return numberoArq
 
 #Localiza as palavras de um texto e retorna uma lista com a posicao de cada palavra
-def achaPosicaoPalavraNoTexto (palavra, arquivoTarget ,address):
-	try:
-		os.system("grep "+palavra+" RD/"+arquivoTarget+" > temp/posicao.txt")
-		arquivoEntrada = open(address+"/temp/posicao.txt", 'r')
-	except:
-		return False
+def achaPosicaoPalavraNoTexto (palavra, arquivoTarget ,address):	
+	import os.path
+	os.system("grep "+palavra+" temp/RD/"+arquivoTarget+" > temp/posicao.txt")
+	arquivoEntrada = open(address+"/temp/posicao.txt", 'r')
 	positionList = []
 	for conteudo_linhaAtual in arquivoEntrada:
 		posicao = conteudo_linhaAtual.rfind("\t")
-		positionList.append(conteudo_linhaAtual[posicao+1:])
+		if (conteudo_linhaAtual[:-2] == "\n"):
+			positionList.append(int(conteudo_linhaAtual[posicao+1:-2]))
+		else:
+			positionList.append(int(conteudo_linhaAtual[posicao+1:]))
 		conteudo_linhaAtual.strip()
 	os.remove(address+"/temp/posicao.txt")
 	return positionList
