5db0c1e5a90d7226daa1ebd60289742af0604ae2
gabrielbecchi <gabrielbecchi97@gmail.com>
2015-08-08 17:25:15 -0300

    versaofinal1

--- DFG/newmain.py
+++ DFG/newmain.py
@@ -17,18 +17,18 @@ args = parser.parse_args()
 minblock = args.minblock
 maxblock = args.maxblock
 desviomax = args.desviomax
-#Excluindo pastas TEMP e RESULTS caso elas existam
-'''
-ESCREVER CODIGO PARA EXCLUIR PASTA TEMP E RESULTS CASO EXISTAM
-'''
-#Localizando o endereco atual
 address = os.getcwd()
-os.makedirs(address + "/temp")
-os.makedirs(address + "/RESULTS")
+#Excluindo pastas TEMP e RESULTS caso elas existam
+if((os.path.exists(address+"/temp"))==True):
+	shutil.rmtree(address + "/temp")
+if((os.path.exists(address+"/RESULTS"))==True):
+	shutil.rmtree(address + "/RESULTS")
 #CriandoPastacomResultados
 if(minblock <= maxblock and minblock > 0):
+	#Localizando o endereco atual
+	os.makedirs(address + "/temp")
+	os.makedirs(address + "/RESULTS")
 	#Descobrindo o numero de arquivos de genomas
-	print(address+"/ENTERFILES")
 	numArquivosEntrada = nameAllfiles(address, "ENTERFILES", "allFilesName")
 	#Iniciando o processamento para cada um dos arquivos
 	if (numArquivosEntrada != 0):
@@ -47,23 +47,23 @@ if(minblock <= maxblock and minblock > 0):
 			allBlocksFiles = open(address + "/temp/allBlocksFiles.txt")
 			for arquivoBlocoAtual in allBlocksFiles:
 				#Descobrindo as palavras Principais
-				palavrasPrincipais(arquivoBlocoAtual, address)
-				listaPalavras = open(address + "/temp/MostUsedWords/"+arquivoBlocoAtual+".txt")
+				palavrasPrincipais(arquivoBlocoAtual[:-1], address)
+				listaPalavras = open(address + "/temp/MostUsedWords_"+arquivoBlocoAtual[:-1])
 				for palavraAtual in listaPalavras:
 					#Achando as ocorrencias da palavra
-					listaPosicoes = achaPosicaoPalavraNoTexto(palavraAtual[:-1], arquivoBlocoAtual, address)
+					listaPosicoes = achaPosicaoPalavraNoTexto(palavraAtual[:-1], arquivoBlocoAtual[:-1], address)
 					numeroDeVezesQueAparece = len(listaPosicoes)
 					#Caso tenha aparecido apenas uma ou duas vezes nao ha como calcular o desvio padrao
 					if (numeroDeVezesQueAparece > 2):
 						devPadrao = numpy.std(listaPosicoes)
-						f.write("***Block (Size/Deplacement): "+arquivoBlocoAtual+", Genome Block: "+palavraAtual+", Std Deviation: "+str(devPadrao)+"\n")
+						f.write("***Block (Size/Deplacement): "+arquivoBlocoAtual[:-1]+", Genome Block: "+palavraAtual[:-1]+", Std Deviation: "+str(devPadrao)+"\n")
 						if (devPadrao <= desviomax):
-							relatorioPrincipal.write("***Genome: "+arquivoAtual+". Block (Size/Deplacement): "+arquivoBlocoAtual+", Genome Block: "+palavraAtual+", Std Deviation: "+str(devPadrao)+"\n")
-					f.write("\n"+"*"*3+"\n")
+							relatorioPrincipal.write("***Genome: "+arquivoAtual[:-1]+", Block (Size/Deplacement): "+arquivoBlocoAtual[:-1]+\
+								", Genome Block: "+palavraAtual[:-1]+", Std Deviation: "+str(devPadrao)+"\n")
+					f.write("\n")
 			shutil.rmtree(address + "/temp/RD")	
-
-
 	else:
 		print("ERRO FINDING THE ENTERFILES, BE SURE ALL FILES IN THE ENTERFILES DIRC ARE GENOME FASTA FILES")
 else:
 	print("THE MINIMUM SIZE MUST BE, AT LEST AS BIG AS THE MAXIMUM SIZE, AND IT CANNOT BE IQUAL TO 0")
+shutil.rmtree(address + "/temp")
