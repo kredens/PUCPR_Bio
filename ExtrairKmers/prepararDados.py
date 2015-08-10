a8df135bd3e25cbe084bdcc860bff74c8bb88fe7
commit_feito <kredens@IRONMAN.local>
2015-08-10 11:04:02 -0300

    as
    as

---
+++ ExtrairKmers/prepararDados.py
@@ -0,0 +1,16 @@
+print ("<INICIO>") 
+import math
+from pymongo import MongoClient
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client.palavras_database
+
+#recuperar kmers com aparição maior que 2
+palavras = db.palavras.findOne()
+for palavra in palavras:
+    print(palavra)
+
+#calcular DP para cada kmer
+
+
+print ("<TERMINO>") 
